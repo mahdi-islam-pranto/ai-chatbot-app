ff5f2d1278cec1a7ee29d4905e0c59bf068c9368
mahdi-islam-pranto <mahdiprantoblog@gmail.com>
2026-04-21 10:51:38 +0600

    Documents upload + chunking + vector store

---
+++ AI Integration/main.py
@@ -0,0 +1,49 @@
+from fastapi import FastAPI, File, UploadFile
+from pydantic import BaseModel, Field
+import os
+from load_document import load_documents
+from rag_pipeline import create_chunks, create_vector_store
+from typing import List, Annotated
+
+app = FastAPI()
+
+class Question(BaseModel):
+    text: str = Field(..., title="The question to ask")
+
+# API endpopint to handle document (files: pdf,txt,docx) uploading
+# API endpoint to handle document (files: pdf, txt, docx) uploading
+@app.post("/upload")
+def upload_documents(files: Annotated[List[UploadFile], File(...)]):
+    documents_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "documents")
+    os.makedirs(documents_dir, exist_ok=True)
+    
+    uploaded_filenames = []
+    
+    for file in files:
+        folder_path = os.path.join(documents_dir, file.filename)
+        with open(folder_path, "wb") as f:
+            f.write(file.file.read())
+        uploaded_filenames.append(file.filename)
+    
+    # load the uploaded documents
+    documents = load_documents(folder_path=documents_dir)
+    
+    # create chunks of documents
+    document_chunks = create_chunks(chunk_size=600, chunk_overlap=150, documents=documents)
+    
+    # create vector store
+    vector_store = create_vector_store(chunks=document_chunks)
+    
+    return {
+        "message": f"{len(uploaded_filenames)} document(s) uploaded successfully.",
+        "uploaded_files": uploaded_filenames,
+        "Length of loaded document pages": len(documents)
+    }
+
+
+# RAG pipeline logic will go here, including loading documents, creating chunks, creating vector store, and creating retriever.
+# API endpopint to handle question answering using the RAG pipeline
+@app.post("/ask")
+def ask_question(question: Question):
+    # Placeholder for the actual RAG pipeline logic
+    return {"response": "This is a placeholder response."}
