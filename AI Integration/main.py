04efdb16e80265eba0181fc92896ff00a91429a2
mahdi-islam-pranto <mahdiprantoblog@gmail.com>
2026-04-25 19:37:14 +0600

    separate two apis

--- AI Integration/main.py
+++ AI Integration/main.py
@@ -1,17 +1,18 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI
 from pydantic import BaseModel, Field
-import os
-from load_document import load_documents
 from searching.hybrid import hybrid_langchain_retriever
-from rag_pipeline import create_chunks, create_vector_store, loaded_vector_store, all_chunks
-from typing import List, Annotated
+from rag_pipeline import  loaded_vector_store
+from build_vector_store import all_chunks
 from langchain_openai import ChatOpenAI
-from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_core.prompts import ChatPromptTemplate
 from dotenv import load_dotenv
 load_dotenv()
 
-app = FastAPI()
+app = FastAPI(
+    title="iHelpBD AI Chatbot API",
+    description="API for question-answering using RAG pipeline with uploaded documents.",
+    version="1.0"
+)
 
 # pydantic class for input
 class Question(BaseModel):
@@ -32,41 +33,22 @@ SYSTEM_PROMPT = """
 
 
 
-# API endpoint to handle document (files: pdf, txt, docx) uploading
-@app.post("/upload")
-def upload_documents(files: Annotated[List[UploadFile], File(...)]):
-    documents_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "documents")
-    os.makedirs(documents_dir, exist_ok=True)
-    
-    uploaded_filenames = []
-    
-    # save uploaded files in the document directory
-    for file in files:
-        folder_path = os.path.join(documents_dir, file.filename)
-        with open(folder_path, "wb") as f:
-            f.write(file.file.read())
-        uploaded_filenames.append(file.filename)
-    
-    # load the uploaded documents
-    documents = load_documents(folder_path=documents_dir)
-    
-    # create chunks of documents
-    document_chunks = create_chunks(chunk_size=600, chunk_overlap=150, documents=documents)
-    
-    # create vector store
-    vector_store = create_vector_store(chunks=document_chunks)
-    
-    return {
-        "message": f"{len(uploaded_filenames)} document(s) uploaded successfully.",
-        "uploaded_files": uploaded_filenames,
-        "Length of loaded document pages": len(documents)
-    }
-
-
-
 # API endpopint to handle question answering using the RAG pipeline
 @app.post("/ask")
 def ask_question(question: Question):
+    
+    try:
+        # check if vector store is loaded
+        if not loaded_vector_store:
+            return {
+                "error": "Vector store not loaded. Please upload documents first."
+            }
+    
+    except Exception as e:
+        return {
+            "error": f"Error loading vector store: {str(e)}. Please upload documents first."
+        }
+    
     # get relevent documents from vector store against the question
     relevant_documents = hybrid_langchain_retriever(
         query=question.text,
@@ -102,6 +84,7 @@ def ask_question(question: Question):
     # Placeholder for the actual RAG pipeline logic
     return {
         "response": results.content,
+        "metadata": results.usage_metadata
         # "retrieved_documents": relevant_documents
         
         }
--- AI Integration/rag_pipeline.py
+++ AI Integration/rag_pipeline.py
@@ -1,42 +1,13 @@
-from load_document import load_documents
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
-from searching.hybrid import hybrid_langchain_retriever
-import os
-
-
-# load documents
-documents_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "documents")
-documents = load_documents(folder_path=documents_dir)
-
-# create document chunker
-def create_chunks(documents, chunk_size=1000, chunk_overlap=200):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap = chunk_overlap)
-    chunks = text_splitter.split_documents(documents=documents)
-    return chunks
-
-all_chunks = create_chunks(documents=documents)
-
-
-
-# create vector store function
-def create_vector_store(chunks):
-    # create embedding model
-    document_embedding_model = HuggingFaceEmbeddings(model_name= "sentence-transformers/all-MiniLM-L6-v2")
-    # create vector store
-    vector_store = FAISS.from_documents(documents=chunks, embedding=document_embedding_model)
-    # store embeddings in a local FAISS 
-    DB_FAISS_PATH="vectorstore/db_faiss"
-    vector_store.save_local(DB_FAISS_PATH)
-    print(f"Vector store created and saved to {DB_FAISS_PATH}")
-    return vector_store
-
+# from searching.hybrid import hybrid_langchain_retriever
 
 
 # load vector store
 DB_FAISS_PATH = "vectorstore/db_faiss"
 document_embedding_model = HuggingFaceEmbeddings(model_name= "sentence-transformers/all-MiniLM-L6-v2")
+
 loaded_vector_store = FAISS.load_local(
     folder_path=DB_FAISS_PATH,
     embeddings=document_embedding_model,
